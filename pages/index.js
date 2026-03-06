5d3be42e591d6bd1e99443fd98342ed15acfc45c
Bruno Corrêa <brunogabrielpk@gmail.com>
2026-03-06 09:46:59 -0300

    fix, v2 of index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1>It should have a nice message here</h1>;
-    return <h1>v2</h1>;
+    return <h1>It should have a nice message here, v2</h1>;
 }
 
 export default Home;
