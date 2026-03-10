999041497c2674acafcfef184642068705a8f4b6
Bruno Corrêa <brunogabrielpk@gmail.com>
2026-03-10 18:33:04 -0300

    prettier lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # curso-dev-test
+
 Implementação de alguma coisa para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>It should have a nice message here, v2</h1>;
+  return <h1>It should have a nice message here, v2</h1>;
 }
 
+function test() {
+  console.log();
+}
 export default Home;
