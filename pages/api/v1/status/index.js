085cd304336abc91a714fd180c8225ef33bcb5ce
brunogabrielpk <brunogabrielpk@gmail.com>
2026-03-22 13:15:42 -0300

    added `database.mjs` and `.enf` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,10 @@
-function status(request, response) {
-  response.status(200).json({"chave" : "Curso.Dev students are awesome.\n Testando caracteres:  ç ã é"})
+import database from "../../../../infra/database.js"
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result)
+  response.status(200).json({ "chave": "Curso.Dev students are awesome.\n Testando caracteres:  ç ã é" });
 }
 
-export default status
+export default status;
 
